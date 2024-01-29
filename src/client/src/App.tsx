7498fdc39e6c3b1c65cfe7d442aa75fea8fe61ea
John Moss <johnmmoss@gmail.com>
2024-01-29 21:07:38 +0000

    Initial commit of client and api projects

---
+++ src/client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+           My Food Tracker 
+      </h1>
+      <p>
+            Coming soon!
+      </p>
+      
+    </div>
+  );
+}
+
+export default App;
